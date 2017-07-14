1cba7491a7359ed27cfbf0b8ba596a5a120bae9d
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-07-14 15:28:04 -0400

    Fixed Pretext

--- DevSpot.js
+++ DevSpot.js
@@ -3,7 +3,7 @@
 
 //components : arduino board - wire board - 2 motors - 2 LED's
 
-// When in test silence other commands i.e. LED Hello world = active, LED disabled
+// When in test silence other commands i.e. LED Hello world = active, LED = disabled
 
 
 //Johnny Five board setup
