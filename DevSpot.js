fce0541247e2b4ab745e07e4d159330612b9f858
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-07-14 15:48:04 -0400

    Updated first statement

--- DevSpot.js
+++ DevSpot.js
@@ -9,7 +9,7 @@
                                             //
                                            //
 
-//Find out how to build Drone-V1
+//Contains : List of requirements - Coding behind Drone-V1
 
 //components : arduino board - wire board - 2 motors - 2 LED's
 
