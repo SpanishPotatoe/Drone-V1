f37afe415d27c499f068b3f5848fa600ae833ffd
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-07-14 12:08:50 -0400

    Test 2

--- DevSpot.js
+++ DevSpot.js
@@ -8,13 +8,5 @@ var board = new five.Board();
 
 board.on("ready", function() {
   var led = new five.Led(13);
-
-  // This will grant access to the led instance
-  // from within the REPL that's created when
-  // running this program.
-  this.repl.inject({
-    led: led
-  });
-
-  led.blink();
+  led.blink(500);
 });
