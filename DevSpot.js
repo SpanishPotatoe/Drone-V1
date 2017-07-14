cdf06e42934711db6a84f0dc129a1da7b7793a10
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-07-14 13:53:10 -0400

    Test 1

--- DevSpot.js
+++ DevSpot.js
@@ -2,3 +2,30 @@
 //Find out how to build Drone-V1
 
 //components : arduino board - wire board - 2 motors - 2 LED's
+
+
+//Johnny Five board setup
+var five = require("johnny-five");
+var board = new five.Board();
+
+//Test code for Arduino : LED Hello world
+// -  var led = new five.Led(13);
+// - led.blink(500);
+});
+
+//Test code for Arduino : LED
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function() {
+  var led = new five.Led(13);
+
+  // This will grant access to the led instance
+  // from within the REPL that's created when
+  // running this program.
+  this.repl.inject({
+    led: led
+  });
+
+  led.blink();
+});
