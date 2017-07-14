f358d19184e7c1ec84f22c4fadd0c2a8cb8780c1
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-07-14 12:11:40 -0400

    Test 3

--- DevSpot.js
+++ DevSpot.js
@@ -2,11 +2,3 @@
 //Find out how to build Drone-V1
 
 //components : arduino board - wire board - 2 motors - 2 LED's
-
-var five = require("johnny-five");
-var board = new five.Board();
-
-board.on("ready", function() {
-  var led = new five.Led(13);
-  led.blink(500);
-});
