93512d4b156a992fb8fa40a19756210928a44130
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-07-14 12:00:41 -0400

    LED test with Elegoo mega 2560 board

--- DevSpot.js
+++ DevSpot.js
@@ -1,6 +1,20 @@
-//Hello SpanishPotatoe
 
 //Find out how to build Drone-V1
 
-//Helo
-//components - a
+//components : arduino board - wire board - 2 motors - 2 LED's
+
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function() {
+  var led = new five.Led(13);
+
+  // This will grant access to the led instance
+  // from within the REPL that's created when
+  // running this program.
+  this.repl.inject({
+    led: led
+  });
+
+  led.blink();
+});
