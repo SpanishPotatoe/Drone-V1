2c38d5626e73cb68fb042de25ae18a9b1aa82425
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-07-14 12:27:59 -0400

    Dev Reset. Test results : board flawed

--- DevSpot.js
+++ DevSpot.js
@@ -2,17 +2,3 @@
 //Find out how to build Drone-V1
 
 //components : arduino board - wire board - 2 motors - 2 LED's
-var five = require("johnny-five");
-var board = new five.Board();
-
-board.on("ready", function() {
-
-  var led = new five.Led(11);
-
-  led.fadeIn();
-
-  // Toggle the led after 5 seconds (shown in ms)
-  this.wait(5000, function() {
-    led.fadeOut();
-  });
-});
