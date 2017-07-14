f1bddd6c4a682c03064543d5cd12558f46f05a99
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-07-14 12:19:42 -0400

    Test 6

--- DevSpot.js
+++ DevSpot.js
@@ -7,18 +7,12 @@ var board = new five.Board();
 
 board.on("ready", function() {
 
-  // Create a standard `led` component
-  // on a valid pwm pin
   var led = new five.Led(11);
 
-  led.pulse();
+  led.fadeIn();
 
-  // Stop and turn off the led pulse loop after
-  // 10 seconds (shown in ms)
-  this.wait(10000, function() {
-
-    // stop() terminates the interval
-    // off() shuts the led off
-    led.stop().off();
+  // Toggle the led after 5 seconds (shown in ms)
+  this.wait(5000, function() {
+    led.fadeOut();
   });
 });
