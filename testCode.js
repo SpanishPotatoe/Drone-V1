a0f977323b6fa3609467ed656ba97a5ac139ccaf
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-07-17 14:44:24 -0400

    Testing if else

--- testCode.js
+++ testCode.js
@@ -4,3 +4,12 @@ function Bob(a,b){
 }
 
 console.log(Bob(1,2), Bob(3050,28), Bob('Daniel ','Cardona'), Bob('Daniel ','is awesome!'));
+
+function PacMan(ghost) {
+if(ghost <= 1) {return 'Holy crap Batman!'
+ } else {
+  return 'Its a trap!'
+  }
+}
+
+console.log( ghost(1) );
