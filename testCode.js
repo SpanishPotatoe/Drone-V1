768bec91c6eebea1e733a1cbf57c7f8dd1cccb46
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-07-17 13:23:56 -0400

    while in () needs to be linear

--- testCode.js
+++ testCode.js
@@ -3,8 +3,4 @@ function Bob(a, b){
   return (a+b);
 }
 
-console.log(
-Bob(1,2)
-Bob(3050,28)
-Bob('Daniel ','Cardona')
-Bob('Daniel ','is awesome!'));
+console.log(Bob(1,2), Bob(3050,28), Bob('Daniel ','Cardona'), Bob('Daniel ','is awesome!'));
