ed449c6db7fad3239a701546bb128ef05f779e43
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-07-17 13:15:38 -0400

    Fixing formating

--- testCode.js
+++ testCode.js
@@ -5,7 +5,4 @@ function Bob(a, b){
 
 console.log(Bob(1,2));
 
-console.log(Bob(3050,28)+ Bob("Daniel ","Cardona"));
-
-
-console.log(Bob("Daniel ","Cardona"));
+console.log(Bob(3050,28)+ Bob(" Daniel ","Cardona"));
