d73cb1b27bd9516ad6d95cc99fe9f2ce364727fd
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-07-18 18:03:29 -0400

    readded matrix removed pulse

--- DevSpot.js
+++ DevSpot.js
@@ -21,11 +21,7 @@ var board = new five.Board();
 
 
 // LED pulse function
-board.on("ready", function() {
-  var array = new five.Leds([3, 5]);
 
-  array.pulse();
-});
 
 // LED REPL commands
 
@@ -42,6 +38,54 @@ board.on("ready", function() {
     },
     off: function() {
       led.off();
+    }
+  });
+});
+
+//LED Matrix
+board.on("ready", function() {
+
+  var heart = [
+    "01100110",
+    "10011001",
+    "10000001",
+    "10000001",
+    "01000010",
+    "00100100",
+    "00011000",
+    "00000000"
+  ];
+
+  var matrix = new five.Led.Matrix({
+    pins: {
+      data: 2,
+      clock: 3,
+      cs: 4
+    }
+  });
+
+  matrix.on();
+
+  var msg = "johnny-five".split("");
+
+  // Display each letter for 1 second
+  function next() {
+    var c;
+
+    if (c = msg.shift()) {
+      matrix.draw(c);
+      setTimeout(next, 1000);
+    }
+  }
+
+  next();
+
+  this.repl.inject({
+    matrix: matrix,
+    // Type "heart()" in the REPL to
+    // display a heart!
+    heart: function() {
+      matrix.draw(heart);
     }
   });
 });
