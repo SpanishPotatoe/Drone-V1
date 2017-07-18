066201582612736b9cfc2993456d3f55e698853c
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-07-18 17:14:42 -0400

    testing commands

--- DevSpot.js
+++ DevSpot.js
@@ -27,53 +27,21 @@ board.on("ready", function() {
   array.pulse();
 });
 
-// LED Matrix
-var five = require("johnny-five");
-var board = new five.Board();
+// LED REPL commands
 
 board.on("ready", function() {
+  console.log("Ready event. Repl instance auto-initialized!");
 
-  var heart = [
-    "01100110",
-    "10011001",
-    "10000001",
-    "10000001",
-    "01000010",
-    "00100100",
-    "00011000",
-    "00000000"
-  ];
-
-  var matrix = new five.Led.Matrix({
-    pins: {
-      data: 2,
-      clock: 3,
-      cs: 4
-    }
-  });
-
-  matrix.on();
-
-  var msg = "johnny-five".split("");
-
-  // Display each letter for 1 second
-  function next() {
-    var c;
-
-    if (c = msg.shift()) {
-      matrix.draw(c);
-      setTimeout(next, 1000);
-    }
-  }
-
-  next();
+  var led = new five.Led(13);
 
   this.repl.inject({
-    matrix: matrix,
-    // Type "heart()" in the REPL to
-    // display a heart!
-    heart: function() {
-      matrix.draw(heart);
+    // Allow limited on/off control access to the
+    // Led instance from the REPL.
+    on: function() {
+      led.on();
+    },
+    off: function() {
+      led.off();
     }
   });
 });
