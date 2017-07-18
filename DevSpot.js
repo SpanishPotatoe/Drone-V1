dadcda7f417a072b6a6f13470347ecfc33d1f9fc
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-07-18 17:19:38 -0400

    test 4

--- DevSpot.js
+++ DevSpot.js
@@ -21,13 +21,18 @@ var board = new five.Board();
 
 
 // LED pulse function
+board.on("ready", function() {
+  var array = new five.Leds([3, 5]);
+
+  array.pulse();
+});
 
 // LED REPL commands
 
 board.on("ready", function() {
   console.log("Ready event. Repl instance auto-initialized!");
 
-  var led = new five.Led([3, 5]);
+  var led = new five.Led(13);
 
   this.repl.inject({
     // Allow limited on/off control access to the
