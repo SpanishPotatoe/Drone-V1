7d98bca07e9cb4661c9522fae6da9ea65d4899e4
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-07-20 10:54:21 -0400

    test 6

--- DevSpot.js
+++ DevSpot.js
@@ -72,19 +72,6 @@ board.on("ready", function() {
   // "calibrated" occurs once, at the beginning of a session,
   motion.on("calibrated", function() {
     console.log("calibrated");
-  });
-
-  // "motionstart" events are fired when the "calibrated"
-  // proximal area is disrupted, generally by some form of movement
-  motion.on("motionstart", function() {
-    console.log("motionstart");
-  });
-
-  // "motionend" events are fired following a "motionstart" event
-  // when no movement has occurred in X ms
-  motion.on("motionend", function() {
-    console.log("motionend");
-
 
     matrix.on();
 
@@ -101,6 +88,19 @@ board.on("ready", function() {
     }
 
 
+  });
+
+  // "motionstart" events are fired when the "calibrated"
+  // proximal area is disrupted, generally by some form of movement
+  motion.on("motionstart", function() {
+    console.log("motionstart");
+
+  });
+
+  // "motionend" events are fired following a "motionstart" event
+  // when no movement has occurred in X ms
+  motion.on("motionend", function() {
+    console.log("motionend");
   });
 
   // "data" events are fired at the interval set in opts.freq
