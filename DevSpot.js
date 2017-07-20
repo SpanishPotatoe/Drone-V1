3f3c7f4e3c8e79a8709f7012d75e05627ecec455
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-07-20 11:09:14 -0400

    test 11

--- DevSpot.js
+++ DevSpot.js
@@ -70,14 +70,6 @@ matrix.on();
 var msg = "Hel lo Daniel".split("");
 
 // Display each letter for 1 second
-function next() {
-  var c;
-
-  if (c = msg.shift()) {
-    matrix.draw(c);
-    setTimeout(next, 1000);
-  }
-}
 
 next();
 
@@ -96,4 +88,44 @@ next();
     }
   });
 
+});
+
+board.on("ready", function() {
+
+  // Create a new `motion` hardware instance.
+  var motion = new five.Motion(7);
+
+  // "calibrated" occurs once, at the beginning of a session,
+  motion.on("calibrated", function() {
+    console.log("calibrated");
+  });
+
+  // "motionstart" events are fired when the "calibrated"
+  // proximal area is disrupted, generally by some form of movement
+  motion.on("motionstart", function() {
+    console.log("motionstart");
+
+    function next() {
+      var c;
+
+      if (c = msg.shift()) {
+        matrix.draw(c);
+        setTimeout(next, 1000);
+      }
+    }
+
+  });
+
+  // "motionend" events are fired following a "motionstart" event
+  // when no movement has occurred in X ms
+  motion.on("motionend", function() {
+    console.log("motionend");
+  });
+
+  // "data" events are fired at the interval set in opts.freq
+  // or every 25ms. Uncomment the following to see all
+  // motion detection readings.
+  // motion.on("data", function(data) {
+  //   console.log(data);
+  // });
 });
