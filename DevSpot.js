2b6471976eb2ed989f583c4bce3c7444b00a1218
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-07-21 13:35:41 -0400

    fix

--- DevSpot.js
+++ DevSpot.js
@@ -112,6 +112,7 @@ board.on("ready", function() {
               "11111111"
             ];
 
+
     var matrix = new five.Led.Matrix({
       pins: {
         data: 2,
@@ -136,7 +137,33 @@ function next() {
 
 //Motion Sensor
 
-  next();
+  // Create a new `motion` hardware instance.
+  var motion = new five.Motion(7);
+
+  // "calibrated" occurs once, at the beginning of a session,
+  motion.on("calibrated", function() {
+    console.log("calibrated");
+  });
+
+  // "motionstart" events are fired when the "calibrated"
+  // proximal area is disrupted, generally by some form of movement
+  motion.on("motionstart", function() {
+    console.log("motionstart");
+    next();
+  });
+
+  // "motionend" events are fired following a "motionstart" event
+  // when no movement has occurred in X ms
+  motion.on("motionend", function() {
+    console.log("motionend");
+  });
+
+  // "data" events are fired at the interval set in opts.freq
+  // or every 25ms. Uncomment the following to see all
+  // motion detection readings.
+  // motion.on("data", function(data) {
+  //   console.log(data);
+  // });
 
   this.repl.inject({
     matrix: matrix,
