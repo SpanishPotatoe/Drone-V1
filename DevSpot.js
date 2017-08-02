ca5877ceefbabc39caf93811f63d2288e74d5da4
SpanishPotatoe <daniel.cardona1998@gmail.com>
2017-08-02 10:55:48 -0400

    testing function timout

--- DevSpot.js
+++ DevSpot.js
@@ -136,14 +136,14 @@ matrix.on();
 var msg = "    Hel lo Daniel ".split("");
 
 // Display each letter for 1 second
-function next() {
+setTimeout(function next() {
   var c;
 
   if (c = msg.shift()) {
     matrix.draw(c);
     setTimeout(next, 1000);
   }
-}
+}, 20000);
 
 //Motion Sensor
 
